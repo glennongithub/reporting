6d56d35498751633913a62d22a74206d5b02e814
Glenn Vinbladh <glenn@sirvoy.com>
2021-10-21 16:19:44 +0200

    next

--- app/www-root/generate_report_card.php
+++ app/www-root/generate_report_card.php
@@ -11,6 +11,28 @@ require_once 'vendor/autoload.php';
 
 use Classes\GeneratePDF;
 
+const LASTNAME = 1;
+const FIRSTNAME = 2;
+const IS_BAPTISED = 3;
+const IS_UNBAPTISED_PUBLISHER = 4;
+const IS_INACTIVE = 5;
+const IS_PIONEER = 6;
+const IS_AUX_PIONEER = 7;
+const CURRENT_GO = 8;
+const IS_ELDER = 9;
+const IS_SERVANT = 10;
+const SEX = 11;
+const BIRTHDATE = 12;
+const BAPTIMSDATE = 13;
+const EMAIL = 14;
+const MOBILE = 15;
+const HOMEPHONE = 16;
+const STREET = 17;
+const CITY = 18;
+const POSTCODE = 19;
+const DIV = 20;
+const MONTHLY_REPORTS = 'monthy_reports';
+
 /*
 $data = [
 
@@ -21,11 +43,29 @@ $data = [
 ];
 */
 
-$data = [
-      'Check Box2' => 'Yes',
-      'Service Year' => '123456',
-      'RemarksJanuary' => 'Some test'
-];
+
+
+
+/**
+ * Takes a array in the format we have for a publisher in kontaktlistan and 
+ * returns the dataArray nneded to fill reportCard with coresponding information 
+ * 
+ */
+function generateNewCardData($publisherArray) {
+      // We need and should always have first last name
+      // Maybe even some more
+      $data = [
+            
+            'Service Year' => '123456',
+            'RemarksJanuary' => 'Some test'
+      ];
+      
+      foreach() {
+            'Check Box1' => $publisherArray[SEX] == 'M' ? 'Yes' : 'No',
+      }
+      
+      
+}
 
 $pdf = new GeneratePdf;
 $response = $pdf->generate($data);
