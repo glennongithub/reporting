a2ea1db4c546938253e0195c6d3e1ff38e223b9b
Glenn Vinbladh <glenn@edgeweb.se>
2021-10-26 15:10:51 +0200

    test

--- app/www-root/generate_report_card.php
+++ app/www-root/generate_report_card.php
@@ -1,4 +1,5 @@
 <?php 
+// This file is not importatn .. and basically removable .now .. we used it for testing initially
 /*
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
      exit;
@@ -52,19 +53,29 @@ $data = [
  * 
  */
 function generateNewCardData($publisherArray) {
-      // We need and should always have first last name
-      // Maybe even some more
-      $data = [
-            
-            'Service Year' => '123456',
-            'RemarksJanuary' => 'Some test'
+      // Some initial data is always expected
+      $formData = [
+            'Name' => $publisherArray[LASTNAME]. ', '. $publisherArray[FIRSTNAME],
+            'Service Year' => '2021/22',
+            // Male or femail
+            'Check Box1' => trim($publisherArray[SEX]) == 'M' ? 'Yes' : 'No',
+            'Check Box2' => trim($publisherArray[SEX]) == 'K' ? 'Yes' : 'No',
+            // Anointed or other cheep
+            'Check Box3' =>'Yes',
+            // 'Check Box4' => 'No' // We have no filed for this right now
+            'Check Box5' => trim($publisherArray[IS_ELDER]) == '1' ? 'Yes' : 'No',
+            'Check Box6' => trim($publisherArray[IS_SERVANT]) == '1' ? 'Yes' : 'No',
+            'Check Box7' => trim($publisherArray[IS_PIONEER]) == '1' ? 'Yes' : 'No',
+            'Date of birth' => trim($publisherArray[BIRTHDATE]),
       ];
-      
-      foreach() {
-            'Check Box1' => $publisherArray[SEX] == 'M' ? 'Yes' : 'No',
-      }
-      
-      
+
+      // Try to handle stupid vatying format of dates
+      $birthDate = new \DateTime(trim($publisherArray[BIRTHDATE]));
+
+      // Batism date we do not touch if we do not have one
+      if (trim($publisherArray[BAPTIMSDATE])) {
+        $formData['Date immersed'] = trim($publisherArray[BAPTIMSDATE])
+      } 
 }
 
 $pdf = new GeneratePdf;
